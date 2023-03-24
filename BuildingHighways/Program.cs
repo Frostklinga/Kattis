ddceb1cdf0ed11b3e3e3a5a9b6fea19417347a84
Frostklinga <daniel@frostklinga.se>
2023-03-24 15:30:37 +0100

    Im tired.

---
+++ BuildingHighways/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using system;
+namespace BuildingHighways
+{
+    internal class Program
+    {
+        static void Main(string[] )        
+    }
+    Console.WriteLine("Hello, World!");
+
+}
